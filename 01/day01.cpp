f2d06d926cd2fbb7cd164a3e394d6a486f916452
jrpinteno <xavi@jrpinteno.me>
2022-12-01 18:15:38 +0200

    Solved day01-part2 Naïve version

--- 01/day01.cpp
+++ 01/day01.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <vector>
+#include <numeric>
 
-int main () {
+std::vector<int> parseInput(const std::filesystem::path& filePath) {
+	std::string currentLine;
+	std::ifstream fileStream{filePath};
+	std::vector<int> inputLines;
+
+	while (std::getline(fileStream, currentLine)) {
+		inputLines.push_back(currentLine.empty() ? 0 : std::stoi(currentLine));
+	}
+
+	return inputLines;
+}
+
+void partOne(std::vector<int>& snacks) {
 	int maxCalories = 0;
 	int elfCalories = 0;
 
-	std::filesystem::path filePath = "input.txt";
-
-	std::ifstream fileStream{filePath};
-	std::string currentLine;
-
-	while (std::getline(fileStream, currentLine)) {
-		if (!currentLine.empty()) {
-			elfCalories += std::stoi(currentLine);
+	for (auto calories : snacks) {
+		if (calories != 0) {
+			elfCalories += calories;
 			continue;
 		}
 
@@ -22,4 +31,30 @@ int main () {
 	}
 
 	std::cout << maxCalories << std::endl;
+}
+
+void partTwo(std::vector<int>& snacks) {
+	int elfCalories = 0;
+	std::vector<int> elves;
+
+	for (auto snack : snacks) {
+		if (snack != 0) {
+			elfCalories += snack;
+			continue;
+		}
+
+		elves.push_back(elfCalories);
+		elfCalories = 0;
+	}
+
+	std::sort(elves.begin(), elves.end(), std::greater<>());
+
+	std::cout << elves[0] + elves[1] + elves[2] << std::endl;
+}
+
+int main () {
+	auto snacks = parseInput("input.txt");
+
+	partOne(snacks);
+	partTwo(snacks);
 }

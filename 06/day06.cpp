f6db612a99ef03a02e2681fce27cda404cbc65f1
jrpinteno <xavi@jrpinteno.me>
2022-12-07 00:07:16 +0200

    Added solution with histograms

--- 06/day06.cpp
+++ 06/day06.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <regex>
 #include <set>
+#include <array>
 
 std::string generateRegex(int window) {
 	std::string regex = "(.)";
@@ -41,13 +42,44 @@ void solveWithSet(const std::string& text, const int window) {
 	}
 }
 
+bool hasRepeatedChars(const std::array<int, 26>& histogram) {
+	return std::any_of(histogram.begin(), histogram.end(), [](const int value) {
+		return value > 1;
+	});
+}
+
+void solveWithHistogram(const std::string& text, const int window) {
+	std::array<int, 26> histogram = { 0 };
+
+	for (int i = 0; i < window; i++) {
+		histogram[text[i] - 'a']++;
+	}
+
+	if (!hasRepeatedChars(histogram)) {
+		std::cout << window + 1 << std::endl;
+		return;
+	}
+
+	for (int i = window; i < text.length(); i++) {
+		histogram[text[i - window] - 'a']--;
+		histogram[text[i] - 'a']++;
+
+		if (!hasRepeatedChars(histogram)) {
+			std::cout << i + 1 << std::endl;
+			return;
+		}
+	}
+}
+
 int main() {
 	auto line = Utils::readString("input06.txt");
 
 	solveWithRegex(line, 4);
 	solveWithSet(line, 4);
+	solveWithHistogram(line, 4);
 	solveWithRegex(line, 14);
 	solveWithSet(line, 14);
+	solveWithHistogram(line, 14);
 }
 
 
